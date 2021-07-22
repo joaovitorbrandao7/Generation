7aaebc2998fb8b065b4b7e9cb66b534419291d2c
joaovitorbrandao7 <joaovitor.brandao21@outlook.com>
2021-07-21 21:35:46 -0300

    Adicionando lista 2

---
+++ MySQL/Lista 2/Exercicio1.sql
@@ -0,0 +1,57 @@
+create database db_game;
+use db_game;
+
+create table classe(
+id bigint(8) auto_increment,
+nome varchar(20) not null,
+equipamento varchar(20) not null,
+alcance varchar(20) not null,
+primary key(id)
+);
+
+INSERT INTO classe (nome, equipamento, alcance) VALUES ('Atirador','Arco e Flexa','A distancia');
+INSERT INTO classe (nome, equipamento, alcance) VALUES ('Atirador','Pistola','Media Distancia');
+INSERT INTO classe (nome, equipamento, alcance) VALUES ('Tanque','Machado','Corpo a Corpo');
+INSERT INTO classe (nome, equipamento, alcance) VALUES ('Tanque','Espada','Corpo a Corpo');
+INSERT INTO classe (nome, equipamento, alcance) VALUES ('Mago','Magia','Media Distancia');
+INSERT INTO classe (nome, equipamento, alcance) VALUES ('Atirador','Besta','A distancia');
+
+create table personagem(
+id bigint(5) auto_increment,
+nome varchar(20) not null,
+regiao varchar(20) not null,
+tipo varchar(20) not null,
+cor_uniforme varchar(10) not null,
+ataque int(10) not null,
+defesa int(10) not null,
+fk_classe bigint(8) not null,
+primary key (id),
+foreign key(fk_classe) references classe(id)
+);
+#ALTER TABLE personagem ADD CONSTRAINT id_classe FOREIGN KEY (id) REFERENCES classe (id);
+
+INSERT INTO personagem (nome,regiao,tipo,cor_uniforme,ataque,defesa,fk_classe) VALUES ('Tristana', 'Bandopolis','Yordle','Azul','1000','500','1');
+INSERT INTO personagem (nome,regiao,tipo,cor_uniforme,ataque,defesa,fk_classe) VALUES ('Twitch', 'Zaun','Hibrido','Verde','900','500','1');
+INSERT INTO personagem (nome,regiao,tipo,cor_uniforme,ataque,defesa,fk_classe) VALUES ('Jhin', 'Ionia','Mutante','Branco','2100','500','2');
+INSERT INTO personagem (nome,regiao,tipo,cor_uniforme,ataque,defesa,fk_classe) VALUES ('Kalista', 'Ilha das Sombras','Fantasma','Azul','900','500','1');
+INSERT INTO personagem (nome,regiao,tipo,cor_uniforme,ataque,defesa,fk_classe) VALUES ('Vayne', 'Demacia','Humana','Azul','900','500','1');
+INSERT INTO personagem (nome,regiao,tipo,cor_uniforme,ataque,defesa,fk_classe) VALUES ('Ekko', 'Zaun','Homem','Verde','2200','1650','5');
+INSERT INTO personagem (nome,regiao,tipo,cor_uniforme,ataque,defesa,fk_classe) VALUES ('Orianna', 'Piltover','Ciborge','Cinza','2750','1500','5');
+INSERT INTO personagem (nome,regiao,tipo,cor_uniforme,ataque,defesa,fk_classe) VALUES ('Camillie', 'Demacia','Ciborgo','Cinzo','2600','2050','3');
+
+SELECT * FROM personagem; 
+
+SELECT * FROM personagem WHERE ataque > 2000;
+SELECT * FROM personagem WHERE defesa >= 1000 AND defesa <= 2000;
+
+SELECT * FROM personagem WHERE nome LIKE "%C%";
+
+SELECT * FROM personagem p
+join classe c
+on p.id = c.id;
+
+SELECT * FROM personagem p
+join classe c
+on p.id = c.id 
+where c.nome like "%Atirador%";	
+
---
+++ MySQL/Lista 2/Exercicio2.sql
@@ -0,0 +1,53 @@
+create database db_pizzaria_legal;
+use db_pizzaria_legal;
+
+create table tb_categoria(
+id bigint(5) auto_increment,
+tamanho varchar(50) not null,
+tipo boolean, #1 salgada, 2 doce
+tipo_massa varchar(50),
+primary key(id)
+);
+
+insert into tb_categoria (tamanho,tipo,tipo_massa) values ('Grande',true,'Fina');
+insert into tb_categoria (tamanho,tipo,tipo_massa) values ('Grande',true,'Grossa');
+insert into tb_categoria (tamanho,tipo,tipo_massa) values ('Média',true,'Fina');
+insert into tb_categoria (tamanho,tipo,tipo_massa) values ('Média',true,'Grossa');
+insert into tb_categoria (tamanho,tipo,tipo_massa) values ('Pequena',true,'Fina');
+insert into tb_categoria (tamanho,tipo,tipo_massa) values ('Pequena',true,'Grossa');
+insert into tb_categoria (tamanho,tipo,tipo_massa) values ('Pequena',false,'Fina');
+
+create table tb_pizza(
+id bigint(5) auto_increment,
+nome varchar(20) not null,
+preco decimal(8,2) not null,
+adicional varchar(20) not null,
+tempero varchar(20) not null,
+produtos varchar(50) not null,
+fk_categoria bigint(8) not null,
+primary key(id),
+foreign key(fk_categoria) references tb_categoria(id));
+
+insert into tb_pizza (nome,preco,adicional,tempero,produtos,fk_categoria) values ('Frango Catupiry','35.00','Catupiry','Oregano','Refri',1);
+insert into tb_pizza (nome,preco,adicional,tempero,produtos,fk_categoria) values ('Calabresa','30.00','Mussarela','Barbecue','Refri',3);
+insert into tb_pizza (nome,preco,adicional,tempero,produtos,fk_categoria) values ('Mussarela','28.00','Sem Adicional','Oregano','Suco',2);
+insert into tb_pizza (nome,preco,adicional,tempero,produtos,fk_categoria) values ('Portuguesa','35.00','Palmito','Milho','Refri',4);
+insert into tb_pizza (nome,preco,adicional,tempero,produtos,fk_categoria) values ('Frango','30.00','Azeitona','Mostarda','Cerveja',2);
+insert into tb_pizza (nome,preco,adicional,tempero,produtos,fk_categoria) values ('Calabresa com Catupiry','30.00','Catupiry','Ketchup','Suco',1);
+insert into tb_pizza (nome,preco,adicional,tempero,produtos,fk_categoria) values ('Chocolate','55.00','Chantily','Coco Ralado','Agua sem Gás',7);
+insert into tb_pizza (nome,preco,adicional,tempero,produtos,fk_categoria) values ('Chocolate Branco','45.00','Granulado','Chantily','Chocolate',7);
+
+select * from tb_pizza where preco >= 45;
+select * from tb_pizza where preco >= 29 and preco <=60;
+select * from tb_pizza where nome like "%C%";
+
+SELECT * FROM tb_pizza p
+join tb_categoria c
+on p.id = c.id;
+
+SELECT * FROM tb_pizza p
+join tb_categoria c
+on p.id = c.id
+where c.tipo = "0";
+
+
