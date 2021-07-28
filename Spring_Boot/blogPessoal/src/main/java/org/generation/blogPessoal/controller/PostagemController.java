e14d99c6bcc794104f118fb5728b8591255d8b7e
joaovitorbrandao7 <joaovitor.brandao21@outlook.com>
2021-07-28 08:08:42 -0300

    Adicionando funcionalidades no blog

--- Spring_Boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ Spring_Boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -7,6 +7,8 @@ import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,22 @@ public class PostagemController {
 	@Autowired
 	private PostagemRepository repository;
 	
+	@GetMapping
 	public ResponseEntity<List<Postagem>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+public ResponseEntity<Postagem> GetById(@PathVariable long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+		
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
 
