ffee3b8197001ecc6bcc82798caa5709fdff9a2c
joaovitorbrandao7 <joaovitor.brandao21@outlook.com>
2021-07-30 09:18:14 -0300

    Atualizacao

--- Spring_Boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ Spring_Boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
