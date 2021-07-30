dc34d7ee819526bc0f811c27518f2d424ddac6e2
joaovitorbrandao7 <joaovitor.brandao21@outlook.com>
2021-07-30 09:33:35 -0300

    Atualização de erro

--- Spring_Boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
+++ Spring_Boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
@@ -39,7 +39,7 @@ public class TemaController {
 	
 	@GetMapping("/nome/{nome}")
 	public ResponseEntity<List<Tema>> getByName(@PathVariable String nome){
-		return ResponseEntity.ok(repository.findAllByDescricaoContrainingIgnoreCase(nome));
+		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(nome));
 	}
 	
 	@PostMapping
--- Spring_Boot/blogPessoal/src/main/java/org/generation/blogPessoal/repository/TemaRepository.java
+++ Spring_Boot/blogPessoal/src/main/java/org/generation/blogPessoal/repository/TemaRepository.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.generation.blogPessoal.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface TemaRepository extends JpaRepository <Tema, Long>{
-	public List<Tema> findAllByDescricaoContrainingIgnoreCase( String descricao);
+	public List<Tema> findAllByDescricaoContainingIgnoreCase( String descricao);
 	
 
 }
